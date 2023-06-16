21b542447bacda74c3803793a7b6d3e7d982f9fe
Dami Olusakin <damiolusakin@squareup.com>
2023-06-16 15:50:50 +0100

    Update actions to remove labels

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const LABELS: { [key: string]: string[] } = {
+  READY_FOR_REVIEW_LABELS: ["ready-for-review"],
+  APPROVED_LABELS: ["ready-for-merge"],
+};
+
+export const LABELS_TO_REMOVE: { [key: string]: string[] } = {
+  READY_FOR_REVIEW_LABELS: ["ready-for-merge"],
+  APPROVED_LABELS: ["ready-for-review"],
+};
--- src/labeler.ts
+++ src/labeler.ts
@@ -3,28 +3,32 @@ import * as github from "@actions/github";
 
 type ClientType = ReturnType<typeof github.getOctokit>;
 
-const LABELS: {[key: string]: string[]} = {
-  READY_FOR_REVIEW_LABELS: ['ready-for-review'],
-  APPROVED_LABELS: ['ready-for-merge']
-}
-
+import { LABELS_TO_REMOVE, LABELS } from "./constants";
 
 export async function run(): Promise<void> {
   try {
     const repoToken = getRepoToken();
     const client = newClient(repoToken);
 
+    await removeLabels(client, getLabelsToRemove());
     await addLabels(client);
   } catch (error: any) {
-    core.error(error)
+    core.error(error);
     if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
+const getLabelsToRemove = (): string[] => {
+  const actionName = getActionType();
+  const labelNameForAction = getLabelNameForAction(actionName);
+  const labelsToRemove = LABELS_TO_REMOVE[labelNameForAction];
+
+  return labelsToRemove;
+};
+
 const getRepoToken = (): string => {
   return core.getInput("repo-token");
 };
-
 
 const newClient = (token: string): ClientType => {
   return github.getOctokit(token);
@@ -34,24 +38,41 @@ const getPrNumber = (): number => {
   return parseInt(core.getInput("pr-number"));
 };
 
-const labelName = (actionType: string): string => {
-  return `${actionType}_LABELS`
-}
+const getLabelNameForAction = (actionType: string): string => {
+  return `${actionType}_LABELS`;
+};
 
 const getLabels = (): string[] => {
   const actionType = getActionType();
 
-  const labels = LABELS[labelName(actionType)]
+  const labels = LABELS[getLabelNameForAction(actionType)];
 
   if (labels) {
-    return labels
+    return labels;
   } else {
-    throw new Error("Unhandled Action Type")
+    throw new Error("Unhandled Action Type");
   }
 };
 
 const getActionType = (): string => {
   return core.getInput("action-type");
+};
+
+const removeLabels = async (client: ClientType, labels: string[]) => {
+  await Promise.all(
+    labels.map((label) => {
+      removeLabel(client, label);
+    })
+  );
+};
+
+const removeLabel = async (client: ClientType, name: string) => {
+  return await client.rest.issues.addLabels({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: getPrNumber(),
+    name,
+  });
 };
 
 const addLabels = async (client: ClientType) => {
